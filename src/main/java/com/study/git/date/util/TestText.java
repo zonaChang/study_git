5348e7ab7cd9ca332c0e610f9de3a78ab7c01f07
changez@thinkive.com <changez@thinkive.com>
2016-08-24 22:08:01 +0800

    add file

---
+++ src/main/java/com/study/git/date/util/TestText.java
@@ -0,0 +1,13 @@
+package com.study.git.date.util;
+/**
+ * @desc:
+ * @author: zona
+ * @time: 2016年8月24日 下午10:07:11
+ */
+public class TestText {
+
+	private String name = "xx";
+	private int age = 2;
+	
+	
+}
