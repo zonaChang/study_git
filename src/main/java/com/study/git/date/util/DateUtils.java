3bd033f5e3d2e0e7a973a4688e3201dd3200bad5
changez@thinkive.com <changez@thinkive.com>
2016-08-24 20:33:02 +0800

    添加时间帮助类

--- src/main/java/com/study/git/date/util/DateUtils.java
+++ src/main/java/com/study/git/date/util/DateUtils.java
@@ -15,6 +15,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class DateUtils {
 
+	
 	private static final String YYYYMMDD = "yyyyMMdd";
 	private static final String YYYY_MM_DD = "yyyy-MM-dd";
 	private static final String YYYYMMDDHH24MMSS = "yyyyMMddHHmmss";
@@ -33,5 +34,21 @@ public class DateUtils {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(operateDate);
 		return calendar.get(Calendar.YEAR);
+	}
+	
+	public static int getCurrentDayOfWeek(String date, String formate) throws ParseException {
+		
+		Date operateDate = changeStr2Date(date, formate);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(operateDate);
+		int current  = calendar.get(Calendar.DAY_OF_WEEK);
+		return current == 1 ? 7 : current -1 ;
+	}
+	
+	private static Date changeStr2Date(String date, String formate) throws ParseException {
+		
+		return  StringUtils.isBlank(date) ? new Date() : //
+			new SimpleDateFormat(StringUtils.isBlank(formate) ? YYYYMMDD : formate)//
+				.parse(date);
 	}
 }
